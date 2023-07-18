27f8e0625cca478ecc5448db1f20197b6f68bcb3
Krishanu-Agarwal <krishanu1599@gmail.com>
2023-07-18 10:17:41 -0400

    Added 2 problems to the MPC problem

--- Mpc_controller.py
+++ Mpc_controller.py
@@ -3,11 +3,18 @@ import numpy as np
 from scipy.optimize import minimize
 
 class MPC_controller():
-    def __init__(self):
+    def __init__(self,x0, x_final, M,P,delT,model, *args,**kwargs):
+        self.x0 = x0
+        self.x_final = x_final #goal position
+        self.P = P #prediciton horizon
+        self.model = model
+        self.args = args
+        self.kwargs = kwargs
         pass
 
     def controller():
-        def objective(): #objective function
+        def objective(u,x_final,x0,h): #objective function
+            return np.sum(np.abs(model(u,x0,h)-x_final))
             pass
 
         def constraint_ineq(): #inequality contraints(non-negative {>=0})
@@ -23,3 +30,5 @@ class MPC_controller():
         return minimize(objective, u,method='SLSQP',constraints=constraints).x[0] #return first value of optimal control
         
 
+def model(u,x0,h):
+    return x0 + h*(np.asarray([[0,1],[0,0]])@x0 +np.asarray([0,1])*u)
